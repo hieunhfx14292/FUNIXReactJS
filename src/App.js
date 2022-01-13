fce5134c1a39c53e2aa5365a91104359aa58972f
Nguyen Hoang Hieu <hieunhfx14292@funix.edu.vn>
2022-01-13 12:08:44 +0700

    Component part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 
-import { Navbar,NavbarBrand } from 'reactstrap';
+import { Media, Navbar,NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color ="primary">
         <div className='container'>
           <NavbarBrand href='/'>Nguyen Hoang Hieu</NavbarBrand>
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
